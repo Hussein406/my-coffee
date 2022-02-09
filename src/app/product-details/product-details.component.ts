3af1c07e871090370166f5ad7bd9a2a1df1ace31
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2022-02-09 15:49:30 -0400

    last edit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,33 @@
+import { Location } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss'],
+})
+export class ProductDetailsComponent implements OnInit {
+  @Input() coffee?: Product; 
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    this.getHero(); 
+  }
+  
+  getHero() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    console.log('id is ', id)
+    this.productService.getCoffee(id).subscribe((coffee) => {
+      console.log('Coffee', coffee)
+    })
+  }
+}
